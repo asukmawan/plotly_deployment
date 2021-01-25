99f95eae5c4d3f602079ca532d504e039f538c88
asukmawan <asukmawan85@gmail.com>
2021-01-24 21:51:14 -0500

    added jumbotron image + bg change to pg and gauge

--- static/js/charts.js
+++ static/js/charts.js
@@ -153,7 +153,9 @@ function buildCharts(sample) {
     var gaugeLayout = { 
       width: 600, 
       height: 500, 
-      margin: { t: 0, b: 0 }
+      margin: { t: 0, b: 0 },
+      paper_bgcolor: "black",
+      font: {color:"white", family: "Arial"}
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
